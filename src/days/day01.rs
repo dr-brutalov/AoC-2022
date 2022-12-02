0115ea847fdf56ee93fd49f2f97d17096e83a0be
dr-brutalov <ccarper2@uwyo.edu>
2022-12-02 16:09:53 -0700

    Basic file i/o

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,10 +1,23 @@
 use crate::{Solution, SolutionPair};
 use std::fs::read_to_string;
+use std::env;
+use std::fs;
+
 
 ///////////////////////////////////////////////////////////////////////////////
 
 pub fn solve() -> SolutionPair {
-    // Your solution here...
+    
+    // File I/O
+    let test_path: &str = "input/01_test.txt";
+    //let input_path: &str = "input/01_input.txt";
+    println!("In file {}", test_path);
+
+    let contents = fs::read_to_string(test_path)
+        .expect("Should have been able to read the file, didn't.");
+
+    println!("With text:\n{contents}");
+
     let sol1: u64 = 0;
     let sol2: u64 = 0;
 
